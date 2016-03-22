0171d4a340320d00fe1789305af8ab8099f55304
nvillanueva <villanueva.arg@gmail.com>
2016-03-22 02:12:45 -0300

    statistics endpoint and redis connection

--- redhog/src/main/java/redhog/Application.java
+++ redhog/src/main/java/redhog/Application.java
@@ -1,14 +1,7 @@
 package redhog;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.io.ClassPathResource;
@@ -18,12 +11,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @RestController
 @SpringBootApplication
 public class Application {
 
-    private String redisHost = System.getenv("REDIS_HOST");
+    private static String redisHost = System.getenv("REDIS_HOST");
     private List<Thread> hogs = new ArrayList<>();
     private List<String> names = loadNames();
 
@@ -51,9 +54,9 @@ public class Application {
      * @param threads Threads to generate in order to consume resources (consider your amount of CPU cores)
      */
     @RequestMapping("/hog")
-    public void hogCpu(@RequestParam(value = "timeout", defaultValue = "10") int timeout,
-                       @RequestParam(value = "threads", defaultValue = "1") int threads)
-        throws IOException, URISyntaxException {
+    public ObjectNode hogCpu(@RequestParam(value = "timeout", defaultValue = "10") int timeout,
+                             @RequestParam(value = "threads", defaultValue = "1") int threads)
+            throws IOException, URISyntaxException {
         Random r = new Random();
         String hogPen = String.valueOf(r.nextInt(Integer.MAX_VALUE));
         List<Thread> localHogs = new ArrayList<>(threads);
@@ -67,19 +70,22 @@ public class Application {
         Sleeper.sleepFor(timeout, "seconds");
         localHogs.parallelStream().forEach(Thread::stop);
         this.hogs.removeAll(localHogs);
+        return JsonNodeFactory.instance.objectNode().put("status", "cool").put("pen", hogPen);
     }
 
     /**
      * Cancel any running CPU hogs
      */
     @RequestMapping("/hog/abort")
-    public void abort() {
+    public ObjectNode abort() {
         this.hogs.parallelStream().forEach(Thread::stop);
         this.hogs.clear();
+        return JsonNodeFactory.instance.objectNode().put("status", "sad").put("reason", "THEY ARE ALL DEAD!");
     }
 
     /**
-     * Simulate a "realistic" query in time and response type. It takes a while, and replies a JSON
+     * Simulate a "realistic" query in time and response type. It takes a while, replies a JSON and may randomly fail.
+     * Just like real life.
      *
      * @param timeout   How long should the request take to return (-1 for a random between 0 and 1000)
      * @param timeunit  Timeunit to use (defaults to milliseconds, but accepts seconds and minutes too)
@@ -89,7 +95,7 @@ public class Application {
     @RequestMapping("/simulate")
     public Response simulateQuery(@RequestParam(value = "duration", defaultValue = "-1") int timeout,
                                   @RequestParam(value = "timeunit", defaultValue = "milliseconds", required = false) String
-                                      timeunit,
+                                          timeunit,
                                   @RequestParam(value = "failChance", defaultValue = "0.0", required = false) float failRatio) {
         Random r = new Random();
         if (timeout == -1) {
@@ -108,6 +114,17 @@ public class Application {
         return new Response(name, String.valueOf(timeout), timeunit, String.valueOf(f));
     }
 
+    /**
+     * Provides statistics of previous runs. Shows the different pig pens each with each pig's name and
+     * the highest prime it found
+     *
+     * @return Statistics with status, reason and serialized stats
+     */
+    @RequestMapping("/stats")
+    public Statistics statistics(@RequestParam(value = "pen", defaultValue = "*", required = false) String pen) {
+        return new Statistics(pen);
+    }
+
     private class Response {
         public String name, time, unit, failIndex;
 
@@ -116,6 +133,34 @@ public class Application {
             this.time = time;
             this.unit = unit;
             this.failIndex = failIndex;
+        }
+    }
+
+    private class Statistics {
+        public String status;
+        public String reason;
+        public ObjectNode pens = JsonNodeFactory.instance.objectNode();
+        private Jedis jedis;
+
+        public Statistics(String requstedPen) {
+            if (Application.redisHost == null) {
+                this.status = "alright";
+                this.reason = "Redis not available";
+                return;
+            }
+            this.jedis = new Jedis(Application.redisHost);
+            this.jedis.keys(requstedPen).forEach(p -> {
+                ObjectNode pen = pens.putObject(p);
+                this.jedis.hgetAll(p).forEach(pen::put);
+            });
+            this.jedis.close();
+            if (pens.size() > 0) {
+                this.status = "awesome";
+                this.reason = "the house is in order";
+            } else {
+                this.status = "eh";
+                this.reason = "Redis is reachable but we have no stats";
+            }
         }
     }
 
